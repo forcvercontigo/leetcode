411033323dbee756bb7cde69951d68f777abc8ad
forcvercontigo <42923955+forcvercontigo@users.noreply.github.com>
2019-02-20 16:25:41 +0800

    Update 002.cpp

--- 002.cpp
+++ 002.cpp
@@ -2,7 +2,7 @@ ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
     ListNode preHead(0), *p = &preHead;
     int extra = 0;
     while (l1 || l2 || extra) {
-        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + extra;
+        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + extra;//added result last time
         extra = sum / 10;
         p->next = new ListNode(sum % 10);
         p = p->next;
