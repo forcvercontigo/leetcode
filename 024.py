2e886d5cbae17748c981bb057925da7060ae5c2e
forcvercontigo <42923955+forcvercontigo@users.noreply.github.com>
2019-02-27 19:23:24 +0800

    Update 024.py

--- 024.py
+++ 024.py
@@ -2,12 +2,14 @@ def swapPairs1(self, head):
     dummy = p = ListNode(0)
     dummy.next = head
     while head and head.next:
+        #swap 
         tmp = head.next
         head.next = tmp.next
         tmp.next = head
         p.next = tmp
+        #move to next node
         head = head.next
-        p = tmp.next
+        p = p.next.next
     return dummy.next
  
 # Recursively    
